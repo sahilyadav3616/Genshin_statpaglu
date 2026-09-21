024b31833d2b0410152748e905f01f96b508315c
Karmanya <sahilyadav3616.1@gmail.com>
2026-09-21 14:36:50 +0530

    SEO: advertise sitemap in Next robots metadata

--- next-app/app/robots.ts
+++ next-app/app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: '*', allow: '/' } };
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://genshin-statpaglu.onrender.com/sitemap.xml',
+  };
 }
